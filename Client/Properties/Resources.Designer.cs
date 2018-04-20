be1aefeede1e2e34b1284737542c8e2e894eb631
Nexus01 <zsj0503@163.com>
2018-04-20 22:16:17 +0800

    change the name

--- Client/Properties/Resources.Designer.cs
+++ Client/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace MiniChat.Properties {
+namespace NanoChat.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace MiniChat.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("MiniChat.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("NanoChat.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
